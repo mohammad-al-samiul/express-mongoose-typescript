fc6561a54a4607f802ea31f1749601bca1982079
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-05-20 21:32:06 +0600

    create student controllers and services

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express'
+import { StudentServices } from './student.service'
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const student = req.body
+    const result = await StudentServices.createStudentIntoDb(student)
+
+    res.status(400).json({
+      success: true,
+      message: 'Student is created successfully',
+      data: result,
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const StudentControllers = {
+  createStudent,
+}
